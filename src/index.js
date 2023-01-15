da0b48454f5e95ae39afa92cde7c58360bb11887
Geovanni dos Santos Correia <geovanni-dsc@hotmail.com>
2023-01-15 14:41:53 -0300

    Criando Botões e configurando Grids

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+<div>
+  <h1>Calculadora</h1>
   <Calculator/>
+</div>
+ 
 );
 
 
