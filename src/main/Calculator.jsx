77119b3df62c58261169fc21579a7641c18bc27e
Geovanni dos Santos Correia <geovanni-dsc@hotmail.com>
2023-01-15 14:52:39 -0300

    Implementação do Display

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import './Calculator.css'
 
+import Display from "../components/Display";
 import Button from "../components/Button";
 
 export default class Calculator extends Component {
@@ -9,6 +10,7 @@ export default class Calculator extends Component {
             return(
                 
                 <div className="calculator">
+                    <Display value={100}/>
                     <Button label="AC"/>
                     <Button label="/"/>
                     <Button label="7"/>
